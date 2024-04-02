46c1e7ca13fd68d31d124d3764ea84a5284b4b47
mandip kamaliya <mandipkamaliya7@gmail.com>
2024-04-02 08:03:29 +0530

    upadated github profie founder

--- Github-profile-finder/src/components/GithubProfileFinder.jsx
+++ Github-profile-finder/src/components/GithubProfileFinder.jsx
@@ -5,24 +5,36 @@ import User from "./user";
 const GithubProfile = () => {
 
     const [username,setusername]=useState("sangammukherjee");
-    const [userData,setuserData]=useState(null);
+    const [userData,setuserData]=useState(" ");
+    const [loading,setloading]=useState(false);
 
         async function fetchGithubuserdata(){
+            setloading(true);
             const res = await fetch(`https://api.github.com/users/${username}`)
             const data = await res.json();
+            if(data){
+                setuserData(data);
+                setloading(false);
+                setusername("")
+            }
 
             console.log(data);
         }
-    function handleclick(){}
+    function handleclick(){
+        fetchGithubuserdata();
+    }
 
     useEffect(()=>{
         fetchGithubuserdata();
     },[]);
+    if(loading){
+        <h1>Loading ! please wait</h1>
+    }
   return <div className="Github-container">
        <h1>Github Profile Finder</h1>
 
        <div className="inpubox">
-       <input  placeholder="enter profile name" type="text" onChange={(e)=>setusername(e.target.value)}></input>
+       <input  placeholder="enter profile name" type="text" onChange={(e)=>setusername(e.target.value)} value={username}></input>
        <button onClick={handleclick}>Search</button>
        </div>
        <User user={userData}></User>
--- Github-profile-finder/src/components/user.jsx
+++ Github-profile-finder/src/components/user.jsx
@@ -1,9 +1,19 @@
 export default function User({user}){
 
-    const {avatar_url,followers,folowing} = User;
+    const {avatar_url,followers,following,login,public_repos,name} = user;
     return <div className="user">
         <div>
-            <img src={avatar_url} alt="user"/>
+            <img src={avatar_url} alt="User"/>
         </div>
+        <a href={`https://github.com/${login}`} >{name || login} </a>
+        <div>
+        <p>followers:{followers}</p>
+        </div>
+       
+        <p>following:{following}</p>
+        <div>
+            public repo : {public_repos}
+        </div>
+        
     </div>
 }
